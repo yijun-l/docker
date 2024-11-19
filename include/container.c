f002c26e21a3e54a3e116ea84f5dc181bc293b74
Yijun <buglittlej@gmail.com>
2024-11-19 22:04:23 +0800

    code for 04 - user namespace

--- include/container.c
+++ include/container.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sched.h>
+#include <string.h>
+#include <fcntl.h>
+
 
 // Print an error message and terminate the program
 void sys_err(const char* msg){
@@ -34,9 +37,45 @@ void ch_hostname(const char* new_hostname){
     printf("%s\n", hostname);
 }
 
+void write_file(const char* filename, const char* content){
+    int fd = open(filename, O_WRONLY);
+
+    if (fd == -1){
+        sys_err("open() failed");
+    }
+
+    int ret = write(fd, content, strlen(content));
+    if ( ret == -1){
+        sys_err("write() failed");
+    }
+
+    close(fd);
+}
+
+void setup_user_namespace(){
+    char buf[64];
+    pid_t current_pid = getpid();
+    printf("before, UID: %d\n", getuid());
+
+    sprintf(buf, "/proc/%d/setgroups", current_pid);
+    write_file(buf, "deny\n");
+
+    sprintf(buf, "/proc/%d/uid_map", current_pid);
+    write_file(buf, "0 0 1\n");
+
+    sprintf(buf, "/proc/%d/gid_map", current_pid);
+    write_file(buf, "0 0 1\n");
+
+     printf("before, UID: %d\n", getuid());
+}
+
 void run_container(){
 
+    // Change hostname
     ch_hostname("new");
+
+    // mapping user to /proc/$$/uid_map
+    setup_user_namespace();
 
     // Start bash
     char *args[] = {"/bin/bash", NULL};
@@ -44,11 +83,13 @@ void run_container(){
         sys_err("execv() failed");
     }
 
+
 }
 
 void run(){
 
-    if (unshare(CLONE_NEWUTS)){
+    // create user/uts namespace
+    if (unshare(CLONE_NEWUTS | CLONE_NEWUSER)){
         sys_err("unshare() failed");
     }
 
